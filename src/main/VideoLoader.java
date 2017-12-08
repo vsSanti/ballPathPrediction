7fb5e299d77657a8df7ca0143355143f165f8acb
Vinícius Santiago Dutra da Silva <viniciussdsilva@gmail.com>
2017-12-08 19:50:32 -0200

    Commit inicial

---
+++ src/main/VideoLoader.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import org.opencv.videoio.VideoCapture;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class VideoLoader {
+
+    private VideoCapture video;
+    private String fileDir;
+    
+    MatParaImagem matParaImagem = new MatParaImagem();
+
+    public VideoLoader() {
+        this.video = new VideoCapture();
+        this.fileDir = System.getProperty("user.dir");
+
+        loadVideo();
+    }
+
+    private void loadVideo() {
+        String first_test = File.separator + "videos" + File.separator + "MVI_2700.MOV";
+        String second_test = File.separator + "teste_2.mp4";
+
+        if (video.open(fileDir + first_test)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+        }
+    }
+
+    public BufferedImage grabFrame() {
+        video.read(matParaImagem.mat);
+        return matParaImagem.getImage(matParaImagem.mat);
+    }
+}
